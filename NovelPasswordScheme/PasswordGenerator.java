cd0dd0d86ca2734f796098dbbd8ceb38d7a22dc6
Mathieu Comeau <Mathieu@172.17.216.83>
2017-03-29 21:01:57 -0400

    fixed formatting and error where passswords would not always contain a number

--- NovelPasswordScheme/PasswordGenerator.java
+++ NovelPasswordScheme/PasswordGenerator.java
@@ -11,77 +11,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public final class PasswordGenerator {
-	
-	private Set<String> words;
-	
-	private String password;
-	
-	private SecureRandom generator;
-	
-	public PasswordGenerator(int wordCount){
-		words = processWords();
-		password = "";
-		generator = new SecureRandom(ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(System.nanoTime()).array());
-		generate(wordCount);
-	}
-	
-	//Getters
-	public String getPassword(){ return password; }
-	
-	private void generate(int desiredWordCount){
-		int WORD_COUNT = words.size(), temp;
-		Object[] WORD_SPACE = words.toArray();
-		boolean hasNumber = false;
-		
-		while (password.length() < 5 && !hasNumber){
-			password = "";
-			hasNumber = false;
-			for(int i = 0; i < desiredWordCount; ++i){
-				if (!hasNumber){
-					if(generator.nextBoolean()){
-						temp = generator.nextInt(100);
-						password += (temp < 10) ? "0" + (Integer.toString(temp)) : Integer.toString(temp);
-						hasNumber = true;
-					}
-				}
-				password += WORD_SPACE[Math.abs(generator.nextInt()) % WORD_COUNT]+" ";
-			}
-			if (!hasNumber){
-				if(generator.nextBoolean()){
-					temp = generator.nextInt(100);
-					password += (temp < 10) ? "0" + (Integer.toString(temp)) : Integer.toString(temp);
-					hasNumber = true;
-				}
-			}
-		}
-	}
-	
-	private static Set<String> parseDictionary(BufferedReader r) throws IOException {
-		Set<String> lines = new HashSet<String>();
+ 
+ private Set<String> words;
+ 
+ private String password;
+ 
+ private SecureRandom generator;
+ 
+ public PasswordGenerator(int wordCount){
+  words = processWords();
+  password = "";
+  generator = new SecureRandom(ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(System.nanoTime()).array());
+  generate(wordCount);
+ }
+ 
+ //Getters
+ public String getPassword(){ return password; }
+ 
+ private void generate(int desiredWordCount){
+  int WORD_COUNT = words.size(), temp;
+  Object[] WORD_SPACE = words.toArray();
+  boolean hasNumber = false;
+  
+  while (password.length() < 5 && !hasNumber){
+   password = "";
+   hasNumber = false;
+   for(int i = 0; i < desiredWordCount; ++i){
+    if (!hasNumber){
+     if(generator.nextBoolean()){
+      temp = generator.nextInt(100);
+      password += (temp < 10) ? "0" + (Integer.toString(temp)) + " " : Integer.toString(temp) + " ";
+      hasNumber = true;
+     }
+    }
+    password += WORD_SPACE[Math.abs(generator.nextInt()) % WORD_COUNT]+" ";
+   }
+   if (!hasNumber){
+     System.out.println("Finished generating words, do not have number");
+     temp = generator.nextInt(100);
+     password += (temp < 10) ? "0" + (Integer.toString(temp)) : Integer.toString(temp);
+     hasNumber = true;
+   }
+  }
+ }
+ 
+ private static Set<String> parseDictionary(BufferedReader r) throws IOException {
+  Set<String> lines = new HashSet<String>();
         for (String line = r.readLine(); line != null; line = r.readLine())
-			if (!lines.contains(line)) lines.add(line);
-		return lines;
-	}
-	
-	private static Set<String> processWords() {
-		Set<String> result;
-		try {
-			BufferedReader r = new BufferedReader(new FileReader("NovelPasswordScheme/words.txt"));
-			long start = System.nanoTime();
-			result = parseDictionary(r);
-			long stop = System.nanoTime();
-			//("Execution time: " + 10e-9 * (stop-start) + "\n");
-			return result;
-		} catch (IOException e) {
-			System.err.println(e);
-			System.exit(-1);
-		}
-		return null;
-	}
-/** TEST USAGE	
-	public static void main(String args[]){
-		PasswordGenerator p = new PasswordGenerator(3);
-		System.out.println(p.getPassword());
-	}
+   if (!lines.contains(line)) lines.add(line);
+  return lines;
+ }
+ 
+ private static Set<String> processWords() {
+  Set<String> result;
+  try {
+   BufferedReader r = new BufferedReader(new FileReader("NovelPasswordScheme/words.txt"));
+   long start = System.nanoTime();
+   result = parseDictionary(r);
+   long stop = System.nanoTime();
+   //("Execution time: " + 10e-9 * (stop-start) + "\n");
+   return result;
+  } catch (IOException e) {
+   System.err.println(e);
+   System.exit(-1);
+  }
+  return null;
+ }
+/** TEST USAGE 
+ public static void main(String args[]){
+  PasswordGenerator p = new PasswordGenerator(3);
+  System.out.println(p.getPassword());
+ }
 **/
 }
